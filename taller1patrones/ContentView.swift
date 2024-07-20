b494aba3ef4ac6d6a75c298392585a00b67e556f
David Fernando Pérez Medina <davidfernando1112@gmail.com>
2024-07-20 10:50:59 -0500

    Initial Commit

---
+++ taller1patrones/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  taller1patrones
+//
+//  Created by David Perez Medina on 20/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ taller1patrones/taller1patronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  taller1patronesApp.swift
+//  taller1patrones
+//
+//  Created by David Perez Medina on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct taller1patronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
